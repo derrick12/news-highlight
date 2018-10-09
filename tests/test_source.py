16eac462c834480d04a31eac2ba0f75a7f1d0085
derrick12 <derrickwaweru@gmail.com>
2018-10-09 15:50:46 +0300

    add a test class to test the behaviour of the Source class

---
+++ tests/test_source.py
@@ -0,0 +1,19 @@
+import unittest
+from app.models import Source
+
+class SourceTest(unittest.Testcase):
+    '''
+    Test class to test the behaviour of the Source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method to run before every test
+        '''
+        self.new_source = Source('Test id','Test name','Test description','Test categoty','Test language'
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))
+
+
+
